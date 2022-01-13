60c122f6e59c11cbef96edf2a8a129fcbc129e20
lauramarcial <95060203+lauramarcial@users.noreply.github.com>
2022-01-12 18:24:54 -0600

    agrege el archivo env

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
+
+require ("dotenv").config();
 
 const app = express();
 
-app.get ('/',function (req, res){res.send('hola mundo');});
+app.get ('/',function (req, res){
+    res.send('hola laura');});
 
-app.listen(3000);
+app.listen(process.env.PORT);
 
-//localhost:3000
+//localhost:3001
